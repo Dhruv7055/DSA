4db2e6d3d9acf74b7853c0ef28303a3285edb407
Dhruv Agarwal <92082181+Dhruv7055@users.noreply.github.com>
2022-06-28 02:14:58 +0530

    Add files via upload

---
+++ isPrime.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (isPrime(n))
+    {
+        cout << " is a prime no." << endl;
+    }
+    else
+    {
+        cout << "Not a prime no." << endl;
+    }
+    return 0;
+}
