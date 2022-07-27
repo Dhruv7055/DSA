23c73e220dc1267a3f876d101b394147266b5fbc
Dhruv Agarwal <92082181+Dhruv7055@users.noreply.github.com>
2022-07-27 15:44:34 +0530

    Add files via upload

---
+++ bubble_sort_using_recursion.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void sortArray(int *arr, int n){
+    if(n == 0 || n == 1){
+        return;
+    }
+    for(int i = 0; i < n-1; i++){
+        if(arr[i] > arr[i+1]){
+            swap(arr[i], arr[i+1]);
+        }
+    }
+    sortArray(arr, n-1);
+}
+
+int main(){
+    int arr[5] = {1, 8, 4, 6, 2};
+    sortArray(arr, 5);
+    
+    for(int i = 0; i < 5; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
