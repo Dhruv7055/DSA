dc2c576de63add7979817070c0df941154f7affd
Dhruv Agarwal <92082181+Dhruv7055@users.noreply.github.com>
2022-08-26 19:25:11 +0530

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the number" << endl;
+    cin >> n;
+
+    int a = 0;
+    int b = 1;
+    cout << a << " " << b << " ";
+    for(int i = 1; i <= n; i++){
+        int nextNumber = a + b;
+        cout << nextNumber << " ";
+        a = b;
+        b = nextNumber;
+    }
+}
