458886fe24f389cdae89714953307899a0338cff
Dhruv Agarwal <92082181+Dhruv7055@users.noreply.github.com>
2022-09-02 18:13:28 +0530

    Add files via upload

---
+++ pattern10.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+/* if n = 4
+   1 
+   2 3
+   3 4 5
+   4 5 6 7  */ 
+int main(){
+    int i = 1;
+    int n;
+    cin >> n;
+    while(i <= n){
+        int j = 1;
+        // int value = i;
+        while(j <= i){
+            cout << i-j+1 << " ";
+            j += 1;
+            // value--;
+        }
+        cout << endl;
+        i += 1;
+    }
+}
